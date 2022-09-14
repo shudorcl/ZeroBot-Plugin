18a038e9b204d0ebe848fedfd7f8dafb819aed68
莫思敛 <55676105+shudorcl@users.noreply.github.com>
2022-09-14 16:50:18 +0800

    惹，写错分支了

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
  set GOOS=linux
  set GOARCH=amd64
- go build -ldflags "-s -w" -o zbp220904 -trimpath
+ set filename=%date:~0,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%%time:~6,2%
+ go build -ldflags "-s -w" -o zbp%filename% -trimpath
